f0d6e5f85f0a0c1546811b484bde1de456e9f862
EnriqueeGC <99845291+EnriqueeGC@users.noreply.github.com>
2026-08-29 15:55:30 -0600

    Simplify CORS configuration in app.js

--- src/app.js
+++ src/app.js
@@ -12,10 +12,7 @@ const errorHandler = require('./middlewares/errorHandler');
 
 const app = express()
 
-app.use(cors({
-  origin: process.env.CLIENT_URL || 'localhost:5173',
-  credentials: true
-}));
+app.use(cors());
 
 app.use(express.json());
 
