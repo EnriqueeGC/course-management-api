3cad1c432a49a0521107ee7783d3e9e28f2b7c0e
edgar <edgargarciafcb@gmail.com>
2026-09-14 23:51:01 -0600

    fix(cors): fix syntax error en cors options

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const authRoutes = require('./routes/auth.routes');
 const errorHandler = require('./middlewares/errorHandler');
 
 const corsOptions = {
-  origin: process.env.CLIENT_URL;
+  origin: process.env.CLIENT_URL,
   credentials: true, 
   optionsSuccessStatus: 200
 };
